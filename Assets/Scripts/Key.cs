1bce0e1962d21a3f5c661608f45f7731637c95c1
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-25 23:46:38 +0100

    Add placeholder music with beats
    and other changes.

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Key : MonoBehaviour, IHitMessageTarget
+{
+    SpriteRenderer m_SpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the SpriteRenderer from the GameObject
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        //Set the GameObject's Color quickly to a set Color (blue)
+        m_SpriteRenderer.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_SpriteRenderer.color = Color.blue;
+    }
+
+    void IHitMessageTarget.Hit()
+    {
+        m_SpriteRenderer.color = Color.red;
+    }
+}
