c94458f1e6b561d0fc664283793ba4bf5fb5ed6c
Filip Bártek <Filip.Bartek@hotmail.com>
2019-01-27 12:15:09 +0100

    Spawn gradually more difficult passerbys

--- Assets/Scripts/PasserbySpawner.cs
+++ Assets/Scripts/PasserbySpawner.cs
@@ -9,6 +9,7 @@ public class PasserbySpawner : MonoBehaviour
     public float radius = 1f;
     public float intervalMin = 1f;
     public float intervalMax = 4f;
+    public float[] times = new float[] { 0f, 0f, 0f, 0f };
 
     static int[][] sequences = {
         new int[] { 0, 1, 0, 1 }, // blue
@@ -17,11 +18,14 @@ public class PasserbySpawner : MonoBehaviour
         new int[] { 3, 2, 1, 0 } // green
     };
 
+    float timeStart;
     float timeNextSpawn;
     
     void Start()
     {
-        timeNextSpawn = Time.time + UnityEngine.Random.Range(intervalMin, intervalMax);
+        timeStart = Time.time;
+        Debug.Assert(Mathf.Min(times) >= 0);
+        timeNextSpawn = Time.time + Mathf.Min(times) + UnityEngine.Random.Range(0, intervalMax - intervalMin);
     }
     
     void Update()
@@ -29,11 +33,28 @@ public class PasserbySpawner : MonoBehaviour
         if (Time.time >= timeNextSpawn)
         {
             timeNextSpawn = Time.time + UnityEngine.Random.Range(intervalMin, intervalMax);
-            Vector2 planarPos = UnityEngine.Random.insideUnitCircle * radius;
-            Vector3 pos = transform.position + new Vector3(planarPos.x, 1, planarPos.y);
-            Passerby passerby = Instantiate(passerbyPrefab, pos, Quaternion.identity);
-            passerby.musician = musician;
-            passerby.SetSequence(new List<int>(sequences[Math.Min((int)UnityEngine.Random.Range(0, sequences.Length), sequences.Length - 1)]));
+            Spawn();
         }
+    }
+
+    List<int> ValidSequences(float timeFromStart)
+    {
+        List<int> result = new List<int>();
+        if (timeFromStart >= times[0]) result.Add(0);
+        if (timeFromStart >= times[1]) result.Add(1);
+        if (timeFromStart >= times[2]) result.Add(2);
+        if (timeFromStart >= times[3]) result.Add(3);
+        return result;
+    }
+
+    void Spawn()
+    {
+        Vector2 planarPos = UnityEngine.Random.insideUnitCircle * radius;
+        Vector3 pos = transform.position + new Vector3(planarPos.x, 1, planarPos.y);
+        Passerby passerby = Instantiate(passerbyPrefab, pos, Quaternion.identity);
+        passerby.musician = musician;
+        var sequenceIndices = ValidSequences(Time.time - timeStart);
+        int sequenceIndex = sequenceIndices[Math.Min((int)UnityEngine.Random.Range(0, sequenceIndices.Count), sequenceIndices.Count - 1)];
+        passerby.SetSequence(new List<int>(sequences[sequenceIndex]));
     }
 }
